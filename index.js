1990172c58fa60b7cb9be846a406063ca52a13b5
Brox0rz <brox0rz@gmail.com>
2023-11-29 17:00:22 -0800

    added mongoose routing and infrastructure for it to function

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
  /*
 ============================================
 ; Title:  index.js
+; Contributing Authors: Professor Richard Krasso, Academind (source: https://www.youtube.com/watch?v=h4A0-53Olm4)
 ; Author: Brock Hemsouvanh
-; Date:   11/11/2023
+; Date:   11/29/2023
 ; Description: Importing modules containing arrays, and functions for a pet care website.
 ;===========================================
 */ 
 
+const Customer = require('./models/customer');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.use(express.static('public'));
 
+const CONN = 'mongodb+srv://web340_admin:204L0bJE7sJJjbQQ@cluster0.1eekj05.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONN).then(() => {  // This section authored by Professor Richard Krasso
+  console.log('Connection to MongoDB database was successful\n  If you see this message it means you were able to connect to your MongoDB Atlas cluster');
+}).catch(err => {
+    console.log('MongoDB Error: ' + err.message);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index'); // Renders views/index.ejs
+    res.render('index'); // Renders views/index.ejs
 });
 
 app.get('/grooming', (req, res) => {
-  res.render('grooming'); // Renders views/grooming.ejs
+    res.render('grooming'); // Renders views/grooming.ejs
 });
 
 app.get('/training', (req, res) => {
-  res.render('training'); // Renders views/training.ejs
+    res.render('training'); // Renders views/training.ejs
 });
 
 app.get('/boarding', (req, res) => {
-  res.render('boarding'); // Renders views/boarding.ejs
+    res.render('boarding'); // Renders views/boarding.ejs
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log(`Server is running on port ${port}`);
+app.get('/register', (req, res) => {
+    res.render('register');
 });
 
+app.post('/register', (req, res) => {
+    const { customerId, email } = req.body;
 
+    Customer.create({ customerId, email })
+        .then(() => res.redirect('/')) // Redirect to the landing page upon success
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Error occurred');
+        });
+});
+
+app.listen(PORT, () => {
+    console.log('Application started and listening on PORT: ' + PORT);
+    console.log('\n  Press Ctrl+C to stop the server...');
+});
